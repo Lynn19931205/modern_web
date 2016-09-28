9f288b542be7bb17531a59847a8219520ad000b1
Lynn19931205 <j890355b@gmail.com>
2016-09-28 15:06:08 +0800

    First Version

---
+++ project_1.js
@@ -0,0 +1,7 @@
+//Each Item should look like this:
+//<li><label>Buy Clothes</label><button class="delete">X</button></li>
+function addTask(){
+    alert('hello world');
+};
+var addBtn = document.getElementById('addBtn');
+addBtn.onClick = addTask();
