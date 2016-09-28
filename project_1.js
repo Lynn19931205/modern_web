366e0308486a6e8e15cb3f5fe499b25adc4c64a6
Lynn19931205 <j890355b@gmail.com>
2016-09-28 22:12:43 +0800

    Second Version

--- project_1.js
+++ project_1.js
@@ -1,7 +1,14 @@
 //Each Item should look like this:
 //<li><label>Buy Clothes</label><button class="delete">X</button></li>
-function addTask(){
-    alert('hello world');
+function addTask() {
+    var listTask = document.createElement("li");
+    listTask.innerText = function input(){
+        document.getElementById("input").submit();
+    };
+    };
+
+    var list = document.getElementById('todoList');
+    list.appendChild(listTask);
 };
 var addBtn = document.getElementById('addBtn');
-addBtn.onClick = addTask();
+addBtn.addEventListener('click', addTask);
